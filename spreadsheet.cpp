3f71461407e1563706d4e7373d94d30be2d39dab
Semyon Klyatsko <klyatskosemyon16@gmail.com>
2020-11-01 22:22:26 +0300

    Spreadsheet creating

---
+++ spreadsheet.cpp
@@ -0,0 +1,140 @@
+#include "spreadsheet.h"
+#include <QDebug>
+#include <QApplication>
+#include <QMessageBox>
+#include <QFile>
+#include <QTableWidget>
+
+
+void Spreadsheet::setHeaders()
+{
+    for(int i = 0; i < columnCount(); ++i)
+    {
+        setHeader(i);
+    }
+}
+
+void Spreadsheet::addRow()
+{
+    QTableWidget::insertRow(rowCount());
+}
+
+void Spreadsheet::addColumn()
+{
+    QTableWidget::insertColumn(columnCount());
+    setHeader(columnCount()-1);
+}
+
+
+void Spreadsheet::setHeader(int col)
+{
+    if(col == 0)
+    {
+        QTableWidgetItem* itm = new QTableWidgetItem("A");
+        setHorizontalHeaderItem(0, itm);
+    } else {
+        auto mod = model();
+        QString prevHead = mod->headerData(col-1,Qt::Orientation::Horizontal).toString();
+        bool allZ = true;
+        for(int i = prevHead.length()-1; i >=0; --i)
+        {
+            if(prevHead[i] != 'Z')
+            {
+               prevHead[i] = QChar(prevHead[i].unicode() + 1);
+               allZ = false;
+               break;
+            } else
+            {
+                prevHead[i] = 'A';
+            }
+        }
+        if(allZ)
+        {
+            QTableWidgetItem* item = new QTableWidgetItem( QString(prevHead.length()+1, 'A'));
+              setHorizontalHeaderItem(col, item);
+        } else {
+
+            QTableWidgetItem* item = new QTableWidgetItem(prevHead);
+              setHorizontalHeaderItem(col, item);
+
+         }
+    }
+}
+
+
+
+Spreadsheet::Spreadsheet(QWidget* parent)
+{
+
+    autoRecalc = true;
+
+      qDebug() << "Spreadsheet";
+    setSelectionMode(ContiguousSelection);
+
+    clear();
+
+    for(int i = 0; i < 10; ++i)
+    {
+        addRow();
+        addColumn();
+    }
+   setHeaders();
+   // connect(this, SIGNAL(itemChanged(QTableWidgetItem*)), this, SLOT(SpreadsheetModified()));
+}
+
+Spreadsheet::~Spreadsheet()
+{
+
+}
+
+
+
+
+void Spreadsheet::recalculate()
+{
+    qDebug() << "Recalculate" << endl;
+
+}
+
+void Spreadsheet::setAutoRecalculation(bool b)
+{
+    autoRecalc = b;
+    if (autoRecalc)
+        recalculate();
+}
+
+void Spreadsheet::SpreadsheetModified()
+{
+    qDebug() << "SpredsheetModified";
+    if(autoRecalc)
+        recalculate();
+    emit modified();
+}
+
+
+
+QString Spreadsheet::currentLocation()
+{
+
+    return QString::number(currentColumn()+1)
+           + QString::number(currentRow() + 1);
+
+}
+
+
+void Spreadsheet::clear()
+{
+    setRowCount(0);
+    setColumnCount(0);
+
+
+
+
+    setCurrentCell(0, 0);
+}
+
+
+
+
+
+
---
+++ spreadsheet.h
@@ -0,0 +1,37 @@
+#ifndef SPREADSHEET_H
+#define SPREADSHEET_H
+
+#include <QTableWidget>
+#include <QObject>
+
+class Spreadsheet : public QTableWidget
+{
+    Q_OBJECT
+
+signals:
+    void modified();
+private:
+    bool autoRecalc;
+
+
+
+public:
+    Spreadsheet(QWidget* parent = nullptr);
+    ~Spreadsheet();
+    QString currentLocation();
+
+    void clear();
+
+public slots:
+    void recalculate();
+    void setAutoRecalculation(bool b);
+    void SpreadsheetModified();
+
+    void setHeaders();
+        void addRow();
+        void addColumn();
+  void setHeader(int col);
+
+};
+
+#endif // SPREADSHEET_H
